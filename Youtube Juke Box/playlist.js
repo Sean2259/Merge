31938249e1f10fdbe09ea6b6a9bacc177e867d31
Sean2259 <saenwoody@gmail.com>
2020-06-27 12:56:53 +0800

    What i have done at this time

--- Youtube Juke Box/playlist.js
+++ Youtube Juke Box/playlist.js
@@ -1,13 +1,13 @@
 let playList = [
-    "4iVW7OhZ0p8", //讓我留在你身邊
-    "MJnzEa0FSKk", //全放空
+    "lZXFxwyTKWE", //讓我留在你身邊
+    "MlJ8-BnuopY", //全放空
     "7NN4RlKspW0" //飄向北方
 ];
 //播放起訖秒數
 let playTime = [
-    [57,60],
-    [79,83],
-    [76,80]  
+    [55,67],
+    [60,76],
+    [70,80]  
 ];
 
 // let playList=[
